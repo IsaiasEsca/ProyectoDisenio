5e3e8901e4c100e546059ca5d57228eb42eb364c
Isaias <19011808@itsoeh.edu.mx>
2023-10-19 00:11:55 -0600

    se mejoro el diseño, de los textFields, se contempla obtener los demas vistas, ya que se obtenga un mejor diseño en la primera vista.

--- app/src/main/java/com/example/proyectodisenio/ui/theme/views/VLoguin/loguin.kt
+++ app/src/main/java/com/example/proyectodisenio/ui/theme/views/VLoguin/loguin.kt
@@ -6,9 +6,10 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 //import androidx.compose.foundation.layout.ColumnScopeInstance.weight
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+//import androidx.compose.foundation.layout.ColumnScopeInstance.weight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -18,23 +19,21 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Lock
 import androidx.compose.material.icons.filled.Person
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MaterialTheme.colorScheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -48,19 +47,19 @@ import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.proyectodisenio.R
-import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.ListItem
-import androidx.compose.material3.OutlinedIconToggleButton
+import androidx.compose.material3.Divider
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.TextFieldColors
 import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.layout.SubcomposeSlotReusePolicy
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.sp
+import com.example.proyectodisenio.ui.theme.md_theme_dark_background
 import com.example.proyectodisenio.ui.theme.views.VLoguin.Vdata.dLoguin
 import com.example.proyectodisenio.ui.theme.views.VLoguin.Vdata.regis
 
@@ -70,6 +69,7 @@ import com.example.proyectodisenio.ui.theme.views.VLoguin.Vdata.regis
 fun LoguinApp(){
     Scaffold(topBar = { LayoutLoguin()}) {it -> LazyColumn(contentPadding = it){
         this.items(regis){
+
             BRegistros(dloguin = it)
         }
     }
@@ -94,39 +94,53 @@ fun LayoutLoguin(modifer: Modifier = Modifier){
            style = TextStyle(fontFamily = FontFamily.Serif))
        Text(text = stringResource(R.string.sub_tittle), fontSize = 20.sp)
 
-        Column(verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally,) {
-            CButt()
-            BInicio()
-        }
+        Body(modifier = Modifier)
+
 
     }
-
 }
+@Composable
+fun Body(modifier: Modifier){
+    var myCorreo by rememberSaveable { mutableStateOf("") }
+    var myContraseña by rememberSaveable { mutableStateOf("") }
+    Column(verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,) {
+
+        TextCorreo(modifier = Modifier, myCorreo = myCorreo, onTextChanged = {myCorreo = it})
+            Spacer(modifier = Modifier.size(16.dp))
+        TextContraseña(modifier = Modifier, myContraseña = myContraseña, onTextChanged = {myContraseña=it})
+        Spacer(modifier = Modifier.size(16.dp))
+        CButt()
+        BInicio()
+        Spacer(modifier = Modifier.size(16.dp))
+        LoguinDivisor()
+    }
+}
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun CButt(modifier: Modifier = Modifier){
+fun TextCorreo(modifier: Modifier, myCorreo: String, onTextChanged: (String) -> Unit ){
     val mediumPadding = dimensionResource(R.dimen.padding_medium)
-    var myText by remember { mutableStateOf("") }
+    val userIcon = Icons.Default.Person
 
-        val userIcon = Icons.Default.Person
-        val ConIcon = Icons.Default.Lock
-Column(
-    modifier = Modifier
-        .fillMaxWidth()
-        .padding(mediumPadding),
-    verticalArrangement =  Arrangement.spacedBy(mediumPadding),
-    horizontalAlignment = Alignment.CenterHorizontally
-
-) {
     TextField(
         modifier = Modifier
             .width(350.dp)
-            .height(60.dp),
-        colors = TextFieldDefaults.textFieldColors(colorResource(id = R.color.white)),
-        value = myText,
-        onValueChange = { myText = it },
+            .height(60.dp)
+            .background(color = Color.Red),
+        colors = TextFieldDefaults.textFieldColors(
+            textColor = Color.Black,
+            containerColor = Color.White,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        ),
+
+        value = myCorreo,
+        onValueChange = { onTextChanged(it) },
         label = { Text(text = "Correo") },
+        maxLines = 1,
+        singleLine = true,
+        keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email),
         leadingIcon = {
             Icon(
                 imageVector = userIcon,
@@ -136,13 +150,25 @@ Column(
             )
         }
     )
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun TextContraseña(modifier: Modifier, myContraseña: String, onTextChanged: (String) -> Unit ){
+    val ConIcon = Icons.Default.Lock
+    val mediumPadding = dimensionResource(R.dimen.padding_medium)
     TextField(
         modifier = Modifier
             .width(350.dp)
             .height(60.dp),
-        colors = TextFieldDefaults.textFieldColors(colorResource(id = R.color.Green)),
-        value = myText,
-        onValueChange = { myText = it },
+        colors = TextFieldDefaults.textFieldColors(
+            textColor = Color.Black,
+            containerColor = Color.White,
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent
+        ),
+        value = myContraseña,
+        onValueChange = { onTextChanged(it) },
         label = { Text(text = "Contraseña") },
         leadingIcon = {
             Icon(
@@ -153,10 +179,16 @@ Column(
             )
         }
     )
+}
+
+
+
+@Composable
+fun CButt(){
     TextButton(onClick = { /*TODO*/ }) {
         Text(text = "¿Olvidaste tu Contraseña?")
     }
-}
+
 }
 @Composable
 fun BInicio(){
@@ -187,9 +219,12 @@ fun BInicio(){
 @Composable
 fun BRegistros(modifier: Modifier = Modifier,
                dloguin: dLoguin
-               ){
+){
+
     Row {
-        Button(onClick = { /*TODO*/ }, modifier = Modifier, colors = ButtonDefaults.buttonColors(colorResource(id = R.color.white)) ) {
+        OutlinedButton(onClick = { /*TODO*/ },
+            modifier = Modifier.padding(horizontal = 10.dp, vertical = 14.dp),
+            colors = ButtonDefaults.buttonColors(colorResource(id = R.color.white)) ) {
             RedIcon(resIcon = dloguin.imageResourceId)
             RedInformation(redInformation = dloguin.parrafo)
         }
@@ -224,5 +259,29 @@ fun RedInformation(modifier: Modifier = Modifier,
            color = Color.Black
         )
 
+    }
+}
+@Composable
+fun LoguinDivisor(){
+    Row (Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically){
+        Divider(
+            Modifier
+                .background(Color.Black)
+                .weight(1f)
+                .height(1.dp)
+        )
+            Text(text = "O",
+                modifier = Modifier.padding(horizontal = 6.dp),
+                fontSize = 12.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Blue
+
+            )
+        Divider(
+            Modifier
+                .background(Color.Black)
+                .weight(1f)
+                .height(1.dp)
+        )
     }
 }
